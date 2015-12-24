6ee459d3ab57b29a5219439e50e17ba2fa36e26c
5chatten <sv89@tut.by>
2015-12-24 21:28:10 +0300

    Update config.cpp
    Fixed appearance of "No owner" messages in RPT

--- CorePatch_CIT_24659/config.cpp
+++ CorePatch_CIT_24659/config.cpp
@@ -10,23 +10,7 @@ class CfgPatches
 };
 class CfgVehicles
 {
-	class LandVehicle;
-	class Car : LandVehicle
-	{
-	};
-	class Wheeled_APC : Car
-	{
-	};
-	class BRDM2_Base : Wheeled_APC
-	{
-	};
-	class BTR60_TK_EP1 : BRDM2_Base
-	{
-		delete hascommander;
-	};
-	class Tank : LandVehicle
-	{
-	};
+	class Tank;
 	class M113_Base : Tank
 	{
 		delete hasCommander;
