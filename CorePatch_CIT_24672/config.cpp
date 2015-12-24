63579b7eb6f4f4a7a29983d5fac0c486aa23b124
5chatten <sv89@tut.by>
2015-12-24 21:30:48 +0300

    Update config.cpp
    Fixed appearance of "No owner" messages in RPT

--- CorePatch_CIT_24672/config.cpp
+++ CorePatch_CIT_24672/config.cpp
@@ -11,13 +11,6 @@ class CfgPatches
 class CfgVehicles
 {
 	class Wheeled_APC;
-	class BRDM2_Base : Wheeled_APC
-	{
-	};
-	class BTR60_TK_EP1 : BRDM2_Base
-	{
-		delete driverOpticsOutModel;
-	};
 	class BTR40_MG_base_EP1 : Wheeled_APC
 	{
 		delete DriverOutOptics;
