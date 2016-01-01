5ed3b413b47d99a430f3f1ebb8310d481bd82e76
5chatten <sv89@tut.by>
2016-01-02 00:55:47 +0300

    Update config.cpp
    Tweaked reload time.
    Removed reload sound.

--- CorePatch_CCP_71037/config.cpp
+++ CorePatch_CCP_71037/config.cpp
@@ -13,18 +13,7 @@ class CfgWeapons
 	class AT5Launcher;
 	class 2A46MRocket : AT5Launcher
 	{
-		magazineReloadTime = 1.5;
-		reloadSound[] = {
-			"\ca\Sounds\weapons\cannon\gun125reload",
-			1,
-			1,
-			20
-		};
-		reloadTime = 5;
-	};
-	class 2A70Rocket : 2A46MRocket
-	{
-		magazineReloadTime = 2;
-		reloadTime = 6;
+		magazineReloadTime = 20;
+		reloadTime = 8;
 	};
 };
