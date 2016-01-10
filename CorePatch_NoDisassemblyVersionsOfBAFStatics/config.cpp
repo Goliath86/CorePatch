6a8db8ae21ecde90740237f9476ac7c073ec032e
ghos7bear <ghost.bear.ru@gmail.com>
2016-01-11 06:07:17 +0700

    No disassembly version should no longer throw messages

--- CorePatch_NoDisassemblyVersionsOfBAFStatics/config.cpp
+++ CorePatch_NoDisassemblyVersionsOfBAFStatics/config.cpp
@@ -8,23 +8,45 @@ class CfgPatches {
 };
 
 class CfgVehicles {
-	class BAF_GPMG_Minitripod_D;
+	class BAF_L2A1_ACOG_base;
+	class BAF_GPMG_Minitripod_D:BAF_L2A1_ACOG_base {
+		class assembleInfo;
+	};
 	class BAF_GPMG_Minitripod_W_NoDisassembly:BAF_GPMG_Minitripod_D {
-		class assembleInfo {};
+		scope = 1;
+		class assembleInfo:assembleInfo {
+			dissasembleTo[] = {};
+		};
 	};
 
-	class BAF_GMG_Tripod_D;
-	class BAF_GMG_Tripod_W_NoDisassembly:BAF_GMG_Tripod_D {
-		class assembleInfo {};
+	class BAF_L2A1_Tripod_D:BAF_L2A1_ACOG_base {
+		class assembleInfo;
 	};
-
-	class BAF_L2A1_Tripod_D;
 	class BAF_L2A1_Tripod_W_NoDisassembly:BAF_L2A1_Tripod_D {
-		class assembleInfo {};
+		scope = 1;
+		class assembleInfo:assembleInfo {
+			dissasembleTo[] = {};
+		};
 	};
 
-	class BAF_L2A1_Minitripod_D;
+	class BAF_L2A1_Minitripod_D:BAF_L2A1_ACOG_base {
+		class assembleInfo;
+	};
 	class BAF_L2A1_Minitripod_W_NoDisassembly:BAF_L2A1_Minitripod_D {
-		class assembleInfo {};
+		scope = 1;
+		class assembleInfo:assembleInfo {
+			dissasembleTo[] = {};
+		};
+	};
+
+	class GMG_TriPod;
+	class BAF_GMG_Tripod_D:GMG_TriPod {
+		class assembleInfo;
+	};
+	class BAF_GMG_Tripod_W_NoDisassembly:BAF_GMG_Tripod_D {
+		scope = 1;
+		class assembleInfo:assembleInfo {
+			dissasembleTo[] = {};
+		};
 	};
 };
