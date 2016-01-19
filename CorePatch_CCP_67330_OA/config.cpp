c961609c89d87d9b56831944298c1a55d64c6fd1
5chatten <sv89@tut.by>
2016-01-19 21:50:00 +0300

    Update config.cpp
    Added new fixes.

--- CorePatch_CCP_67330_OA/config.cpp
+++ CorePatch_CCP_67330_OA/config.cpp
@@ -43,3 +43,91 @@ class CfgVehicles
 		displayName = "$STR_DN_V3S_REFUEL_TK_GUE_EP1_CP";
 	};
 };
+class CfgWeapons
+{
+	class AK_107_GL_kobra;
+	class AK_107_GL_pso : AK_107_GL_kobra
+	{
+		displayName = "$STR_DN_AK_107_GL_PSO_CP";
+	};
+	class M4A1;
+	class M4A1_RCO_GL : M4A1
+	{
+		displayName = "$STR_DN_M4A1_RCO_GL_CP";
+
+		class M4_ACOG_Muzzle : M4A1
+		{
+			displayName = "$STR_DN_M4A1_RCO_GL_CP";
+		};
+	};
+	class M24;
+	class M40A3 : M24
+	{
+		descriptionShort = "$STR_DSS_M40A3_CP";
+	};
+	class M240;
+	class m240_scoped_EP1 : M240
+	{
+		displayName = "$STR_DN_M240_SCOPED_EP1_CP";
+	};
+	class M249;
+	class M249_m145_EP1 : M249
+	{
+		displayName = "$STR_DN_M249_M145_EP1_CP";
+	};
+	class Rifle;
+	class AA12_PMC : Rifle
+	{
+		descriptionShort = "$STR_DSS_AA12_CP";
+	};
+	class G36C : Rifle
+	{
+		displayName = "$STR_DN_G36C_CP";
+	};
+	class G36_C_SD_eotech : G36C
+	{
+		displayName = "$STR_DN_G36C_SD_CP";
+	};
+	class G36a : Rifle
+	{
+		displayName = "$STR_DN_G36A_CP";
+
+		class MuzzleFar : G36C
+		{
+			displayName = "$STR_DN_G36A_CP";
+		};
+	};
+	class G36K : G36a
+	{
+		displayName = "$STR_DN_G36K_CP";
+
+		class MuzzleFar : MuzzleFar
+		{
+			displayName = "$STR_DN_G36K_CP";
+		};
+	};
+	class M60A4_EP1 : Rifle
+	{
+		descriptionShort = "$STR_DSS_M60A4_EP1_CP";
+	};
+	class SCAR_H_CQC_CCO;
+	class SCAR_H_STD_EGLM_Spect : SCAR_H_CQC_CCO
+	{
+		descriptionShort = "$STR_DSS_SCAR_H_STD_EGLM_SPECT_CP";
+	};
+	class SCAR_L_Base;
+	class SCAR_L_STD_Mk4CQT : SCAR_L_Base
+	{
+		descriptionShort = "$STR_DSS_SCAR_L_STD_MK4CQT_CP";
+	};
+	class SCAR_L_STD_EGLM_RCO;
+	class SCAR_L_STD_EGLM_TWS : SCAR_L_STD_EGLM_RCO
+	{
+		descriptionShort = "$STR_DSS_SCAR_L_STD_EGLM_TWS_CP";
+	};
+	class Sa58V_EP1;
+	class Sa58V_CCO_EP1 : Sa58V_EP1
+	{
+		displayName = "$STR_DN_SA58V_CCO_EP1_CP";
+	};
+};
