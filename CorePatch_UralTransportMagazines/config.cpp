ef6ce1d04caa37a870cc039e6a36c20769a4615d
ghos7bear <ghost.bear.ru@gmail.com>
2016-05-12 00:40:22 +0700

    Ural cargo fixes v2

--- CorePatch_UralTransportMagazines/config.cpp
+++ CorePatch_UralTransportMagazines/config.cpp
@@ -16,7 +16,17 @@ class CfgVehicles {
 		transportMaxMagazines = 1000;
 	};
 	class Ural_ZU23_Base:Ural_Base_withTurret	{class TransportMagazines {};};
+	class Ural_ZU23_CDF:Ural_ZU23_Base		{class TransportMagazines {};};
+	class Ural_ZU23_INS:Ural_ZU23_Base		{class TransportMagazines {};};
+	class Ural_ZU23_Gue:Ural_ZU23_Base		{class TransportMagazines {};};
+	class Ural_ZU23_TK_EP1:Ural_ZU23_Base		{class TransportMagazines {};};
+	class Ural_ZU23_TK_GUE_EP1:Ural_ZU23_Base	{class TransportMagazines {};};
 	class GRAD_Base:Ural_Base_withTurret		{class TransportMagazines {};};
+	class GRAD_CDF:GRAD_Base			{class TransportMagazines {};};
+	class GRAD_RU:GRAD_Base				{class TransportMagazines {};};
+	class GRAD_INS:GRAD_Base			{class TransportMagazines {};};
+	class GRAD_TK_EP1:GRAD_Base			{class TransportMagazines {};};
+	class RM70_ACR:GRAD_Base			{class TransportMagazines {};};
 	class UralRepair_Base:Ural_Base			{class TransportMagazines {};};
 	class UralRefuel_Base:Ural_Base			{class TransportMagazines {};};
 	class UralCivil:Ural_Base			{class TransportMagazines {};};
