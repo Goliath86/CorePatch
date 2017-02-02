acc2dad9f97421f28ca8eeb5a78117335c48cf40
ghos7bear <ghost.bear.ru@gmail.com>
2017-02-02 12:53:32 +0700

    MQ9 Reaper fixes

--- CorePatch_CCP_70991/config.cpp
+++ CorePatch_CCP_70991/config.cpp
@@ -14,16 +14,45 @@ class CfgVehicles
 	class UAV : Plane
 	{
 		class NewTurret;
+		class ViewOptics;
+		class ViewPilot;
 	};
-	class MQ9PredatorB : UAV
-	{
-		class Turrets
-		{
-			class MainTurret : NewTurret
-			{
+	class MQ9PredatorB: UAV {
+		model = "\Corepatch\CorePatch_CCP_70991\MQ9PredatorB.p3d";
+		driverForceOptics = 1;
+		driverOpticsModel = "\ca\weapons\optika_empty";
+		selectionRotorStill = "propellerStatic";
+		selectionRotorMove = "propellerBlurred";
+		class Turrets {
+			class MainTurret: NewTurret {
 				gunBeg = "laser_end";
 				gunEnd = "laser_start";
+				minElev = -89;
+				maxElev = 26;
+				initElev = 0;
+				minTurn = -180;
+				maxTurn = 180;
+				initTurn = 0;
 			};
+		};
+		class ViewOptics: ViewOptics {
+			initFov = 0.85;
+			maxFov = 0.35;
+			minFov = 0.95;
+			visionMode[] = {
+				"Normal",
+				"NVG"
+			};
+		};
+		class ViewPilot: ViewPilot {
+			minAngleX = -65;
+			maxAngleX = 85;
+			minAngleY = -150;
+			maxAngleY = 150;
+
+			initFov = 0.8;
+			minFov = 0.3;
+			maxFov = 1;
 		};
 	};
 };
