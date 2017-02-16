e01bd6bde9cea61aa64093c2b5740b8ba60ae843
ghos7bear <ghost.bear.ru@gmail.com>
2017-02-16 18:20:47 +0700

    Mi-24 fixes

--- CorePatch_CIT_8198/config.cpp
+++ CorePatch_CIT_8198/config.cpp
@@ -8,21 +8,74 @@ class CfgPatches
 		weapons[] = {};
 	};
 };
-class CfgVehicles
-{
-	class Helicopter;
-	class Mi24_Base: Helicopter
-	{
-		model = "\CorePatch\CorePatch_CIT_8198\models\Mi24_V";
+class CfgVehicles {
+	class Air;
+	class Helicopter:Air {
+		class Turrets;
+		class HitPoints {
+			class HitEngine;
+			class HitGlass2;
+			class HitGlass4;
+		};
 	};
-	class Mi24_Base_CDF;
-	class Mi24_D: Mi24_Base_CDF
-	{
-		model = "\CorePatch\CorePatch_CIT_8198\models\Mi35";
+	class Mi24_Base:Helicopter {
+		armor = 50;
+		model = "\CorePatch\CorePatch_CIT_8198\models\Mi24_V.p3d";
+		class Turrets:Turrets {
+			class MainTurret;
+		};
+		class HitPoints:HitPoints {
+			class HitEngine:HitEngine {
+				armor = 0.9;
+			};
+		};
 	};
-	class Mi24_Base_RU;
-	class Mi24_P: Mi24_Base_RU
-	{
-		model = "\CorePatch\CorePatch_CIT_8198\models\Mi24_P";
+	class Mi24_Base_RU:Mi24_Base {};
+	class Mi24_P:Mi24_Base_RU {
+		model = "\CorePatch\CorePatch_CIT_8198\models\Mi24_P.p3d";
+		class Turrets:Turrets {
+			class MainTurret:MainTurret {
+				body = "mainTurret";
+				gun = "mainGun";
+				gunBeg = "muzzle_2";
+				gunEnd = "chamber_2";
+				memoryPointGun = "muzzle_2";
+			};
+		};
+	};
+	class Mi24_V:Mi24_Base_RU {};
+	class Mi24_V2:Mi24_V {
+		scope = 1;
+		model = "\CorePatch\CorePatch_CIT_8198\models\Mi24_V2.p3d";
+		class Turrets:Turrets {
+			class MainTurret:MainTurret {
+				weapons[] = {"YakB", "AT9Launcher"};
+				magazines[] = {"1470Rnd_127x108_YakB", "8Rnd_AT9_Mi24V"};
+			};
+		};
+	};
+	class Mi24_Base_CDF:Mi24_Base {};
+	class Mi24_D:Mi24_Base_CDF {
+		model = "\CorePatch\CorePatch_CIT_8198\models\Mi35.p3d";
+	};	
+	class Mi24_Base_TK_EP1:Mi24_Base {};
+	class Mi24_D_Base_TK_EP1:Mi24_Base_TK_EP1 {
+		model = "\CorePatch\CorePatch_CIT_8198\models\Mi35.p3d";
+	};
+	class Mi24_Base_CZ_ACR:Mi24_Base {};
+	class Mi24_D_Base_CZ_ACR:Mi24_Base_CZ_ACR {
+		model = "\CorePatch\CorePatch_CIT_8198\models\mi35_ACR.p3d";
+	};
+};
+class CfgMagazines {
+	class 4Rnd_AT9_Mi24P;
+	class 8Rnd_AT9_Mi24V:4Rnd_AT9_Mi24P {
+		count = 8;
+	};
+};
+class CfgWeapons {
+	class MissileLauncher;
+	class AT9Launcher:MissileLauncher {
+		magazines[] = {"4Rnd_AT9_Mi24P", "8Rnd_AT9_Mi24V"};
 	};
 };
